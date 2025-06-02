e306c715e0068439c6474be06487c385a37ff3ab
hkscsheph <shepherdng@creativehk.edu.hk>
2025-06-02 08:00:14 +0800

    add: models

--- src/stores/models.ts
+++ src/stores/models.ts
@@ -75,13 +75,13 @@ const modelData: Model[] = [
   },
   {
     id: '2B02',
-    name: '面具',
-    description: '面具',
+    name: '怪人',
+    description: '怪人',
     url: '/Cheung Kai Long 2B02.gltf',
     reference: null,
     thumbnailUrl: '/Cheung Kai Long 2B02.png',
     designer: 'Cheung Kai Long',
-    category: '面具'
+    category: '怪人'
   },
   {
     id: '2B03',
@@ -332,6 +332,216 @@ const modelData: Model[] = [
     thumbnailUrl: '/Tong Kin Wing 2R13.png',
     designer: 'Tong Kin Wing',
     category: '唐三藏'
+  },
+  {
+    id: '2B15',
+    name: '哪吒',
+    description: '哪吒',
+    url: '/Tsang Kai Ho 2B15.gltf',
+    reference: null,
+    thumbnailUrl: '/Tsang Kai Ho 2B15.png',
+    designer: 'Tsang Kai Ho',
+    category: '哪吒'
+  },
+  {
+    id: '2G14',
+    name: '女人',
+    description: '女人',
+    url: '/Tse Hoi Tung 2G14.gltf',
+    reference: null,
+    thumbnailUrl: '/Tse Hoi Tung 2G14.png',
+    designer: 'Tse Hoi Tung',
+    category: '女人'
+  },
+  {
+    id: '2B16',
+    name: '怪人',
+    description: '怪人',
+    url: '/Tse Wai Cheng 2B16.gltf',
+    reference: null,
+    thumbnailUrl: '/Tse Wai Cheng 2B16.png',
+    designer: 'Tse Wai Cheng',
+    category: '怪人'
+  },
+  {
+    id: '2R14',
+    name: '怪人',
+    description: '怪人',
+    url: '/Tse Yu Ki 2R14.gltf',
+    reference: null,
+    thumbnailUrl: '/Tse Yu Ki 2R14.png',
+    designer: 'Tse Yu Ki',
+    category: '怪人'
+  },
+  {
+    id: '2B17',
+    name: '玉皇大帝',
+    description: '玉皇大帝',
+    url: '/Tse Yu Yan 2B17.gltf',
+    reference: null,
+    thumbnailUrl: '/Tse Yu Yan 2B17.png',
+    designer: 'Tse Yu Yan',
+    category: '玉皇大帝'
+  },
+  {
+    id: '2G15',
+    name: '骷髏兵',
+    description: '骷髏兵',
+    url: '/Wong Chi Yin 2G15.gltf',
+    reference: null,
+    thumbnailUrl: '/Wong Chi Yin 2G15.png',
+    designer: 'Wong Chi Yin',
+    category: '骷髏兵'
+  },
+  {
+    id: '2R15',
+    name: '和尚',
+    description: '和尚',
+    url: '/Wong Chor Wing 2R15.gltf',
+    reference: null,
+    thumbnailUrl: '/Wong Chor Wing 2R15.png',
+    designer: 'Wong Chor Wing',
+    category: '和尚'
+  },
+  {
+    id: '2R16',
+    name: '諸葛孔明',
+    description: '諸葛孔明',
+    url: '/Wong Miu 2R16.gltf',
+    reference: null,
+    thumbnailUrl: '/Wong Miu 2R16.png',
+    designer: 'Wong Miu',
+    category: '諸葛孔明'
+  },
+  {
+    id: '2B18',
+    name: '銀角',
+    description: '銀角',
+    url: '/Wong Nozomi 2B18.gltf',
+    reference: null,
+    thumbnailUrl: '/Wong Nozomi 2B18.png',
+    designer: 'Wong Nozomi',
+    category: '銀角'
+  },
+  {
+    id: '2G22',
+    name: '仙女',
+    description: '仙女',
+    url: '/Wong Oi Kiu 2G22.gltf',
+    reference: null,
+    thumbnailUrl: '/Wong Oi Kiu 2G22.png',
+    designer: 'Wong Oi Kiu 2G22',
+    category: '仙女'
+  },
+  {
+    id: '2R17',
+    name: '戰神呂布',
+    description: '戰神呂布',
+    url: '/Wong Yin Nam 2R17.gltf',
+    reference: null,
+    thumbnailUrl: '/Wong Yin Nam 2R17.png',
+    designer: 'Wong Yin Nam',
+    category: '戰神呂布'
+  },
+  {
+    id: '2B19',
+    name: '孫悟空',
+    description: '孫悟空',
+    url: '/Yeung Joreson 2B19.gltf',
+    reference: null,
+    thumbnailUrl: '/Yeung Joreson 2B19.png',
+    designer: 'Yeung Joreson',
+    category: '孫悟空'
+  },
+  {
+    id: '2B20',
+    name: '哪吒',
+    description: '哪吒',
+    url: '/Yeung Ngai Tung 2B20.gltf',
+    reference: null,
+    thumbnailUrl: '/Yeung Ngai Tung 2B20.png',
+    designer: 'Yeung Ngai Tung',
+    category: '哪吒'
+  },
+  {
+    id: '2G17',
+    name: '美杜莎',
+    description: '美杜莎',
+    url: '/Yeung Tsz Yau 2G17.gltf',
+    reference: null,
+    thumbnailUrl: '/Yeung Tsz Yau 2G17.png',
+    designer: 'Yeung Tsz Yau',
+    category: '美杜莎'
+  },
+  {
+    id: '2G18',
+    name: '門神',
+    description: '門神',
+    url: '/Yip Dick Yar Naddy 2G18.gltf',
+    reference: '/Yip Dick Yar Naddy 2G18.jpg',
+    thumbnailUrl: '/Yip Dick Yar Naddy 2G18.png',
+    designer: 'Yip Dick Yar Naddy',
+    category: '門神'
+  },
+  {
+    id: '2G19',
+    name: '女人',
+    description: '女人',
+    url: '/Yip Macy Chung Kiu 2G19.gltf',
+    reference: null,
+    thumbnailUrl: '/Yip Macy Chung Kiu 2G19.png',
+    designer: 'Yip Macy Chung Kiu',
+    category: '女人'
+  },
+  {
+    id: '2B21',
+    name: '哪吒',
+    description: '哪吒',
+    url: '/Yiu Sze Nga 2B21.gltf',
+    reference: null,
+    thumbnailUrl: '/Yiu Sze Nga 2B21.png',
+    designer: 'Yiu Sze Nga',
+    category: '哪吒'
+  },
+  {
+    id: '2B23',
+    name: '豬八戒',
+    description: '豬八戒',
+    url: '/Yuen Cheuk Ling 2B23.gltf',
+    reference: null,
+    thumbnailUrl: '/Yuen Cheuk Ling 2B23.png',
+    designer: 'Yuen Cheuk Ling',
+    category: '豬八戒'
+  },
+  {
+    id: '2G21',
+    name: '守財奴',
+    description: '守財奴',
+    url: '/Yuen Ching Hin 2G21.gltf',
+    reference: null,
+    thumbnailUrl: '/Yuen Ching Hin 2G21.png',
+    designer: 'Yuen Ching Hin',
+    category: '守財奴'
+  },
+  {
+    id: '2R19',
+    name: '老鼠精',
+    description: '老鼠精',
+    url: '/Yung Ho Muhammad Numan 2R19.gltf',
+    reference: null,
+    thumbnailUrl: '/Yung Ho Muhammad Numan 2R19.png',
+    designer: 'Yung Ho Muhammad Numan',
+    category: '老鼠精'
+  },
+  {
+    id: '2R20',
+    name: '孫悟空',
+    description: '孫悟空',
+    url: '/Zhou Ching Hin Angus 2R20.gltf',
+    reference: null,
+    thumbnailUrl: '/Zhou Ching Hin Angus 2R20.png',
+    designer: 'Zhou Ching Hin Angus',
+    category: '孫悟空'
   },
 ]
 
